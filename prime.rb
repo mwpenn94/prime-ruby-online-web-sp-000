0ab6aed29dba65a47781ff01c25a2ed9a0466d26
Michael Penn <mwpenn94@gmail.com>
2020-12-30 00:39:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(n)  
-  divisors = (2..(n-1)).to_a 
-  divisors.none? {|div| n % div == 0}
-  if n < 1
+  if n < 1 
     return false
-  end
+  else
+    divisors = (2..(n-1)).to_a 
+    divisors.none? {|div| n % div == 0}
 end
