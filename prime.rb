e92594e00cb3d5fbe80c1c868fa87665af1275b5
Michael Penn <mwpenn94@gmail.com>
2020-12-30 00:32:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,5 @@
 # Add  code here!
 def prime?(n)  
   divisors = (1..(n-1).to_a 
-  divisors.any? {|div| n % div == 0}
-  if true
-    return 
-    return true
-  else 
-    return false
+  divisors.none? {|div| n % div == 0}
   end
